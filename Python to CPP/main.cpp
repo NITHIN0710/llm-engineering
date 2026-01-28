5f2e8fe33a740d25e9cceabeafeeee37a799ee9e
NITHIN <vnithin0704@gmail.com>
2026-01-28 13:11:03 +0530

    Updated Gradio UI

--- Python to CPP/main.cpp
+++ Python to CPP/main.cpp
@@ -1,28 +1,34 @@
-#include <bits/stdc++.h>
-using namespace std;
+#include <iostream>
+#include <iomanip>
+#include <chrono>
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    const int iterations = 200000000;   // 200 million
+    const int param1 = 4;
+    const int param2 = 1;
 
-    const int iterations = 200'000'000;
-    const double param1 = 4.0;
-    const double param2 = 1.0;
-
-    auto start = chrono::high_resolution_clock::now();
+    auto start = std::chrono::high_resolution_clock::now();
 
     double result = 1.0;
+    int j1 = param1 - param2;   // 3
+    int j2 = param1 + param2;   // 5
+
     for (int i = 1; i <= iterations; ++i) {
-        double i4 = i * param1;
-        result -= 1.0 / (i4 - param2);
-        result += 1.0 / (i4 + param2);
+        result -= 1.0 / j1;
+        result += 1.0 / j2;
+        j1 += param1;           // increment by 4 each loop
+        j2 += param1;
     }
+
     result *= 4.0;
 
-    auto end = chrono::high_resolution_clock::now();
-    double elapsed = chrono::duration<double>(end - start).count();
+    auto end = std::chrono::high_resolution_clock::now();
+    std::chrono::duration<double> elapsed = end - start;
 
-    cout << fixed << setprecision(12) << "Result: " << result << "\n";
-    cout << fixed << setprecision(6) << "Execution Time: " << elapsed << " seconds\n";
+    std::cout << std::fixed << std::setprecision(12)
+              << "Result: " << result << '\n';
+    std::cout << std::fixed << std::setprecision(6)
+              << "Execution Time: " << elapsed.count() << " seconds\n";
+
     return 0;
 }
